bf394d2839297b405c6f97bb6daf31bb1e3b222e
Reddy <kulreddy@deloitte.com>
2022-04-12 13:25:51 +0530

    nlb

---
+++ 15-AWS-NLB-Network-Load-Balancer/terraform-manifests/c1-versions.tf
@@ -0,0 +1,29 @@
+# Terraform Block
+terraform {
+  #required_version = "~> 0.14"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+    null = {
+        source = "hashicorp/null"
+        version = "~> 3.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region = var.aws_region
+  profile = "default"
+}
+
+# Create Random Pet Resource
+resource "random_pet" "this" {
+  length = 2
+}
