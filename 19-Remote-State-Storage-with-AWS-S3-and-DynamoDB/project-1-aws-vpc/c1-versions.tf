df80af23095b0c7298cecda242bb81de4b96c31e
Reddy <kulreddy@deloitte.com>
2022-04-13 06:12:22 +0530

    remote s3 and dynamodb

---
+++ 19-Remote-State-Storage-with-AWS-S3-and-DynamoDB/project-1-aws-vpc/c1-versions.tf
@@ -0,0 +1,26 @@
+# Terraform Block
+terraform {
+  #required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "terraformaws-forec2"
+    key    = "dev/project1-vpc/terraform.tfstate"
+    region = "us-east-1" 
+
+    # Enable during Step-09     
+    # For State Locking
+    dynamodb_table = "dev_project1_vpc"    
+  }    
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
